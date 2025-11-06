4fc61116603b06a94782d9fd21eff03c85b1c666
Kaue <kaue@kaue.com>
2025-11-06 15:06:53 -0300

    ajuste das rotas API para ambiente local e online

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,13 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432,
-  ssl: { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.connect()
-  .then(() => console.log('✅ Conexão com PostgreSQL (Neon) bem-sucedida!'))
-  .catch((err) => console.error('❌ Erro ao conectar ao PostgreSQL:', err));
+  .then(() => console.log("✅ Conexão com PostgreSQL (Neon) bem-sucedida!"))
+  .catch(err => console.error("❌ Erro ao conectar ao PostgreSQL:", err));
 
 module.exports = pool;
